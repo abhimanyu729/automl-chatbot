2839b7b015bd5f679ccac13fe7220bda00ba1aa8
Abhimanyu Anand <63829842+abhimanyu729@users.noreply.github.com>
2024-08-22 14:26:43 -0400

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,11 +1,3 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
-
-
-
 from typing import Any, Text, Dict, List, Union
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
